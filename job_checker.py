38caa5900d869e738433ea424762146b9fdefb4e
MyDevOpsJourney77 <sonawanekundan777@gmail.com>
2026-03-27 00:14:27 +0530

    Create job_checker.py

---
+++ job_checker.py
@@ -0,0 +1,85 @@
+import requests
+
+# ====== YOUR DETAILS ======
+BOT_TOKEN = "import requests
+
+# ====== YOUR DETAILS ======
+BOT_TOKEN = "8460172500:AAG9hHAaVi9CsAP0Vdje9dDA_LhyZ8XCHbw"
+CHAT_ID = "880622327"
+# ==========================
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{8460172500:AAG9hHAaVi9CsAP0Vdje9dDA_LhyZ8XCHbw}/sendMessage"
+    data = {
+        "chat_id": 880622327,
+        "text": message
+    }
+    requests.post(url, data=data)
+
+def check_jobs():
+    url = "https://www.naukri.com/devops-engineer-jobs"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if "DevOps Engineer" in response.text:
+        send_message("🚀 New DevOps Job Found on Naukri!\nhttps://www.naukri.com/devops-engineer-jobs")
+
+if __name__ == "__main__":
+    check_jobs()"
+CHAT_ID = "880622327"
+# ==========================
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{import requests
+
+# ====== YOUR DETAILS ======
+BOT_TOKEN = "8460172500:AAG9hHAaVi9CsAP0Vdje9dDA_LhyZ8XCHbw"
+CHAT_ID = "880622327"
+# ==========================
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{8460172500:AAG9hHAaVi9CsAP0Vdje9dDA_LhyZ8XCHbw}/sendMessage"
+    data = {
+        "chat_id": 880622327,
+        "text": message
+    }
+    requests.post(url, data=data)
+
+def check_jobs():
+    url = "https://www.naukri.com/devops-engineer-jobs"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if "DevOps Engineer" in response.text:
+        send_message("🚀 New DevOps Job Found on Naukri!\nhttps://www.naukri.com/devops-engineer-jobs")
+
+if __name__ == "__main__":
+    check_jobs()}/sendMessage"
+    data = {
+        "chat_id": 880622327,
+        "text": message
+    }
+    requests.post(url, data=data)
+
+def check_jobs():
+    url = "https://www.naukri.com/devops-engineer-jobs"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if "DevOps Engineer" in response.text:
+        send_message("🚀 New DevOps Job Found on Naukri!\nhttps://www.naukri.com/devops-engineer-jobs")
+
+if __name__ == "__main__":
+    check_jobs()
